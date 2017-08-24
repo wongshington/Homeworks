bc6b1312952a12b8e50496af4646b5b4878af124
Jesse Wong <jnwong17@gmail.com>
2017-08-23 22:56:58 -0700

    Finish w4d4 hw

---
+++ W4D4/hw_project/app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+  validates :username, :session_token, :password_digest, presence: true
+  validates :password, length: {minimum: 6, allow_nill: true}
+
+  attr_reader :password
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+  end
+
+  def find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+end
